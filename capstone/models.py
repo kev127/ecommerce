554a6c848dd036ed9ed96e02c0d216dc4de67436
kev127 <kelvinkeya125@gmail.com>
2020-12-07 20:39:40 +0300

    add recent works model

--- capstone/models.py
+++ capstone/models.py
@@ -6,3 +6,10 @@ class Service(models.Model):
 
     def __str__(self):
         return self.name
+
+class RecentWork(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="media")
+
+    def __str__(self):
+        return self.title
