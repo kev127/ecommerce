10b5c3b071da6af617a0dfc46e38becf2ac98eb5
kev127 <kelvinkeya125@gmail.com>
2020-12-09 15:56:54 +0300

    update view function

--- capstone/views.py
+++ capstone/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from .models import Service, RecentWork, Product, Customer, Order
 
 # Create your views here.
 def welcome(request):
@@ -6,6 +7,7 @@ def welcome(request):
 
 def home(request):
     return render(request, 'all-capstone/home.html')
+
 
 def about(request):
     return render(request, 'all-capstone/about.html')
@@ -20,6 +22,67 @@ def contacts(request):
     return render(request, 'all-capstone/contacts.html')
 
 def shop(request):
-    context = {}
-    return render(request, 'all-capstone/shop.html')
+    def get(self,request):
+		cart = request.session.get('cart')
+		products = Product.getAllProduct().order_by('-id')
 
+		if request.GET.get('id'):
+			filterProductById = Product.objects.get(id=int(request.GET.get('id')))
+			return render(request, 'all-capstone/products.html',{"product":filterProductById})
+
+		if not cart:
+			request.session['cart'] = {}
+
+		
+		return render( request, 'all-capstone/shop.html',{"products":products)
+
+	def post(self,request):
+		product = request.POST.get('product')
+
+		cart = request.session.get('cart')
+		if cart:
+			quantity = cart.get(product)
+			if quantity:
+				cart[product] = quantity+1
+			else:
+				cart[product] = 1
+		else:
+			cart = {}
+			cart[product] = 1
+
+		print(cart)
+		request.session['cart'] = cart
+		return redirect('cart')
+
+class OrderView(View):
+	def get(self,request):
+		customer_id = request.session.get('customer')
+		orders = Order.objects.filter(customer=customer_id).order_by("-date").order_by("-id")
+		print(orders)
+		return render(request,'order.html',{"orders":orders})
+
+class Checkout(View):
+	def get(self,request):
+		return redirect('cart')
+	
+	def post(self,request):
+		address = request.POST.get('address')
+		phone = request.POST.get('phone')
+		cart = request.session.get('cart')
+		products = Product.getProductById(list(cart.keys()))
+		customer = request.session.get('customer')
+		print(address,phone,cart,products,customer)
+
+		for product in products:
+			newOrder = Order(
+					product=product,
+					customer=Customer(id=customer),
+					quantity=cart[str(product.id)],
+					price=product.price,
+					address=address,
+					phone=phone,
+				)
+			newOrder.save()
+
+		request.session['cart'] = {}
+		return redirect('order')
