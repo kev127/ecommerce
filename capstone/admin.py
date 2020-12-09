3f48138bebc2a901679450054c5e59fade909b2b
kev127 <kelvinkeya125@gmail.com>
2020-12-09 15:59:01 +0300

    update admin.py file

--- capstone/admin.py
+++ capstone/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Service,RecentWork
+from .models import Service,RecentWork,Customer,Order,Product
 
 admin.site.register(Service)
 admin.site.register(RecentWork)
+admin.site.register(Product,AdminProduct)
+admin.site.register(Customer)
+admin.site.register(Order)
