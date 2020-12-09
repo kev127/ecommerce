8f5cd6bc3481a59f9ce142a65258862133a06ed9
kev127 <kelvinkeya125@gmail.com>
2020-12-09 21:39:27 +0300

    add registration form

--- kabutech/urls.py
+++ kabutech/urls.py
@@ -19,4 +19,5 @@ from django.urls import include,path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('capstone.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
 ]
