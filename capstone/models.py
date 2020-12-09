45f244a2b2344b757b9e1cc6b970f24a74ca00af
kev127 <kelvinkeya125@gmail.com>
2020-12-09 15:29:17 +0300

    add class order model

--- capstone/models.py
+++ capstone/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 class Service(models.Model):
     name = models.CharField(max_length=100)
@@ -56,3 +57,17 @@ class Customer(models.Model):
 			return email
 		except:
 			return False
+
+class Order(models.Model):
+	product = models.ForeignKey(Product,on_delete=models.CASCADE)
+	customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+	quantity = models.IntegerField(default=1)
+	price = models.IntegerField()
+	date = models.DateField(default=datetime.datetime.today)
+	address = models.CharField(max_length=255,blank=True)
+	phone = models.CharField(max_length=15,blank=True)
+	completed = models.BooleanField(default=False)
+
+	
+	def __str__(self):
+		return self.customer.email
