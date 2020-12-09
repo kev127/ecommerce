960f4c83839127ea7b7d3b83f3d17d3e3d2bea2d
kev127 <kelvinkeya125@gmail.com>
2020-12-09 08:49:46 +0300

    add shop,cart,checkout templates

--- capstone/urls.py
+++ capstone/urls.py
@@ -1,11 +1,13 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url('home/',views.home,name = 'home'),
-    url('about/',views.about,name = 'about'),
-    url('service/',views.service,name = 'service'),
-    url('recent/',views.recent,name = 'recent'),
+    path('',views.home,name = 'home'),
+    path('home/',views.home,name = 'home'),
+    path('about/',views.about,name = 'about'),
+    path('service/',views.service,name = 'service'),
+    path('recent/',views.recent,name = 'recent'),
+    path('conatcts/',views.contacts,name = 'contacts'),
 ]
