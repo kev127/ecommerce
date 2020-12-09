5d00a5f8465935251f595f3c3c4379611ec82ee9
kev127 <kelvinkeya125@gmail.com>
2020-12-09 15:22:59 +0300

    update code, add product model

--- capstone/models.py
+++ capstone/models.py
@@ -13,3 +13,28 @@ class RecentWork(models.Model):
 
     def __str__(self):
         return self.title
+
+class Product(models.Model):
+	name = models.CharField(max_length=220)
+	price = models.IntegerField(default=0)
+	description = models.TextField()
+	image = models.ImageField(upload_to='upload/products')
+	date = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.name
+
+	@staticmethod
+	def getAllProduct():
+		return Product.objects.all()
+
+	@staticmethod
+	def getProductByFilter(category_id):
+		if category_id:
+			return Product.objects.filter(category = category_id)
+		else:
+			return Product.getAllProduct()
+
+	@staticmethod
+	def getProductById(productList):
+		return Product.objects.filter(id__in=productList)
